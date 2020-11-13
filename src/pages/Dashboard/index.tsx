d3a423f155576653dcd53bfd1ecd6c7374d3d6d1
Jose Francisco de Almeida Filho <69639482+josefralmeidaads@users.noreply.github.com>
2020-11-13 14:38:16 -0300

    Projeto de visualização de repositorio do github, foi adicionado estilo global e rotas na aplcação

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+    return (
+        <h1>Explore repositórios no Github</h1>
+    )
+}
+
+export default Dashboard;
---
+++ src/pages/Repositories/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Repositories: React.FC = () => {
+  return (
+      <h1>Repositories</h1>
+  );
+}
+
+export default Repositories;
